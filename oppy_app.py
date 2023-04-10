d3155e661df6d0a7d3aaf8caac6a6dec25b91fa1
ARAKI1223 <araki1223taka@gmail.com>
2023-04-10 21:57:11 +0900

    test

--- oppy_app.py
+++ oppy_app.py
@@ -7,13 +7,12 @@ from matplotlib.animation import ArtistAnimation
 from matplotlib.animation import FuncAnimation
 from IPython import display
 
-def x_set(t, y, R18_flag):
+def x_set(t, y, R18_flag, cap):
     
     # x1 = 1.5*np.exp((0.12*np.sin(t)-0.5)*(y+0.16*np.sin(t))**2)/(1+np.exp(-20*(5*y+np.sin(t))))
     # x2 = (1.5+0.8*(y+0.2*np.sin(t))**3)*(1+np.exp(20*(5*y+np.sin(t))))**(-1)/(1+np.exp(-1*(100*(y+1)+16*np.sin(t))))
     # x3 = 0.2*(np.exp(-1*(y+1)**2)+1)/(1+np.exp(100*(y+1)+16*np.sin(t)))
     # x4 = 0.1/np.exp(2*(10*y+1.2*(2+np.sin(t))*np.sin(t))**4)
-    cap = 1
     x1 = 1.5*cap*np.exp((0.12*np.sin(t)-0.5)*(y+0.16*np.sin(t))**2)/(1+np.exp(-20*(5*y+np.sin(t))))
     x2 = cap*(1.5+0.8*(y+0.2*np.sin(t))**3)*(1+np.exp(20*(5*y+np.sin(t))))**(-1)/(1+np.exp(-1*(100*(y+1)+16*np.sin(t))))
     x3 = 0.2*cap*(np.exp(-1*(y+1)**2)+1)/(1+np.exp(100*(y+1)+16*np.sin(t)))
